29d3dc691fb097a11f6eab212ff64228e12ee29d
Leonardo Pinzon <leonardo3103@gmail.com>
2020-08-13 20:18:08 -0300

    update model pokemon with all data and default image

--- src/client/jsx/components/api/pokemon.js
+++ src/client/jsx/components/api/pokemon.js
@@ -3,9 +3,19 @@ export default class {
         this.name = data.name;
         this.id = data.id;
         this.types = data.types;
-        this.img = data.img;
+        this.img = data.img || "https://e7.pngegg.com/pngimages/993/363/png-clipart-pokeball-pokeball.png";
+        this.height = data.height;
+        this.weight = data.weight;
+        this.abilities = data.abilities;
+        this.heightM = this.getConvertHW(this.height);
+        this.weightK = this.getConvertHW(this.weight);
         this.getNumberPokemon(this.id);
     }
+
+    getConvertHW(value) {
+        return value / 10
+    }
+
     getNumberPokemon(id) {
         let number = "#";
         if (id < 10) {
