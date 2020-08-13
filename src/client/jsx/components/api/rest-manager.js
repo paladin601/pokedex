8ee807d3ebbcb78c65418aa72b57ed6677f80666
Leonardo Pinzon <leonardo3103@gmail.com>
2020-08-13 20:18:54 -0300

    update pokemon that save data and update method for get pokemon for id

--- src/client/jsx/components/api/rest-manager.js
+++ src/client/jsx/components/api/rest-manager.js
@@ -11,7 +11,7 @@ export default class {
     async findAll() {
         if (!this.pokemons.length > 0) {
             return Promise.all([
-                await fetch(this.baseURL + "?limit=1000")
+                await fetch(this.baseURL + "?limit=100")
                     .then(res => res.json())
                     .then(async data => {
                         let result = data.results
@@ -19,7 +19,7 @@ export default class {
                             await scope.findPokemonData(result[i].url);
                         }
                     })
-            ]).then(async () => {
+            ]).then(() => {
                 return scope.pokemons;
             })
         } else {
@@ -39,6 +39,9 @@ export default class {
                     id: data.id,
                     types: data.types,
                     img: data.sprites.front_default,
+                    height: data.height,
+                    weight: data.weight,
+                    abilities: data.abilities
                 })
                 );
             });
@@ -48,11 +51,49 @@ export default class {
 
     }
     async getPokemonByID(id) {
-        let pokemon = this.pokemons.filter(obj => obj.id === id);
-        if (!pokemon) {
-            await this.findPokemonData(this.baseURL + "/" + id);
-            pokemon = this.pokemons.filter(obj => obj.id === id);
+        let pokemon = this.pokemons.filter(obj => obj.id == id);
+        if (!pokemon.length > 0) {
+            return Promise.all([]).then(() => {
+                return {
+                    error: true
+                }
+            })
+        } else {
+            return Promise.all([]).then(() => {
+                let index = scope.pokemons.findIndex(obj => obj == pokemon[0]);
+                return {
+                    pokemon: pokemon[0],
+                    prePokemon: scope.getNextPrevPokemon(index)
+                };
+            })
         }
-        return pokemon;
+    }
+
+    getNextPrevPokemon(id) {
+        let out = [];
+        let max = this.pokemons.length - 1;
+        let next = id + 1;
+        let prev = id - 1;
+        if (id < 1) {
+            out.push(this.getNumberIdPokemon(max));
+            out.push(this.getNumberIdPokemon(next));
+        } else {
+            if (id == max) {
+                out.push(this.getNumberIdPokemon(prev));
+                out.push(this.getNumberIdPokemon(0));
+            } else {
+                out.push(this.getNumberIdPokemon(prev));
+                out.push(this.getNumberIdPokemon(next));
+            }
+        }
+
+        return out;
+    }
+
+    getNumberIdPokemon(id) {
+        return {
+            id: this.pokemons[id].id,
+            number: this.pokemons[id].number
+        }
     }
 } 
