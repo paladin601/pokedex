94b4d2d893feda350587088253b0b334274f5080
Leonardo Pinzon <leonardo3103@gmail.com>
2020-08-17 14:28:06 -0300

    update fetch all pokemons

--- src/client/jsx/components/api/rest-manager.js
+++ src/client/jsx/components/api/rest-manager.js
@@ -8,49 +8,50 @@ export default class {
         this.pokemons = [];
     }
 
-    async findAll() {
+    findAll() {
         if (!this.pokemons.length > 0) {
-            return Promise.all([
-                await fetch(this.baseURL + "?limit=1000")
+            return new Promise((resolve, reject) => {
+                fetch(this.baseURL + "?limit=1000")
                     .then(res => res.json())
-                    .then(async data => {
+                    .then(data => {
                         let result = data.results
+                        let max = result.length;
                         for (let i = 0; i < result.length; i++) {
-                            await scope.findPokemonData(result[i].url);
+                            fetch(result[i].url)
+                                .then(res => res.json())
+                                .then(data => {
+                                    //scope.pokemons.push(data);
+                                    scope.pokemons.push(new Pokemon({
+                                        name: data.name,
+                                        id: data.id,
+                                        types: data.types,
+                                        img: data.sprites.front_default,
+                                        height: data.height,
+                                        weight: data.weight,
+                                        abilities: data.abilities
+                                    })
+                                    );
+                                    if (scope.pokemons.length == max) {
+                                        scope.pokemons.sort((a, b) => (a.id > b.id) ? 1 : -1);//sort
+                                        resolve(scope.pokemons);
+                                    }
+                                });
                         }
                     })
-            ]).then(() => {
-                return scope.pokemons;
+            }).then((data) => {
+                return data;
             })
         } else {
-            return Promise.all([]).then(() => {
-                return scope.pokemons;
+            return new Promise((resolve, reject) => {
+                resolve(scope.pokemons)
+            }).then((data) => {
+                return data;
             })
         }
 
     }
-    async findPokemonData(url) {
-        await fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                //scope.pokemons.push(data);
-                scope.pokemons.push(new Pokemon({
-                    name: data.name,
-                    id: data.id,
-                    types: data.types,
-                    img: data.sprites.front_default,
-                    height: data.height,
-                    weight: data.weight,
-                    abilities: data.abilities
-                })
-                );
-            });
-    }
 
-    getPokemonByName(name) {
-
-    }
-    async getPokemonByID(id) {
+    getPokemonByID(id) {
         let pokemon = this.pokemons.filter(obj => obj.id == id);
         if (!pokemon.length > 0) {
             return Promise.all([]).then(() => {
